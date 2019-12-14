796da19bf940f9416f782b58e5350462b255869c
Paul Leugemors <pleugemors@yahoo.com>
2019-12-14 10:42:42 +0000

    Automatically backed up by Learn

--- lib/concerns/paramable.rb
+++ lib/concerns/paramable.rb
@@ -1,7 +1,16 @@
 module Paramable
   
   def to_param
-    
+    name.downcase.gsub(' ', '-')
   end
+  
+  class Artist 
+    include Paramable
+  end
+  
+  class Song 
+    include Paramable
+  end
+    
   
 end
